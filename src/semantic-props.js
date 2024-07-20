7c110c30c4d4c81531ab76c7515be24b199dbf32
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-07-19 23:23:35 -0600

    Changed custom property classes to is- attributes to fix issues in JavaScript frameworks.

--- src/semantic-props.js
+++ src/semantic-props.js
@@ -4,81 +4,77 @@ if (typeof window !== "undefined") {
 	class SemanticProps extends HTMLElement {
 		constructor() {
 			super();
-	
+
 			// Create element shadow DOM:
 			const shadow = this.attachShadow({ mode: "open" });
-	
+
 			// Create content slot element:
 			const slot = document.createElement("slot");
-	
+
 			// Add slot to component:
 			shadow.appendChild(slot);
 		}
-	
-		/** Adds appropriate breakpoint class for current container width. @type {ResizeObserver} */
+
+		/** Adds appropriate breakpoint attributes for current container width. @type {ResizeObserver} */
 		static breakpoint = new ResizeObserver(entries => {
-			/** Names of custom property or class breakpoints. @type {string[]} */
-			const breakpoints = [
-				"--2xs-breakpoint", "--xs-breakpoint",
-				"--sm-breakpoint", "--md-breakpoint", "--lg-breakpoint",
-				"--xl-breakpoint", "--2xl-breakpoint", "--3xl-breakpoint"
-			];
-	
-			// Loop through all semantic-props elements adding classes:
+			/** Names of breakpoint is attributes. @type {string[]} */
+			const breakpoints = ["2xs", "xs", "sm", "md", "lg", "xl", "2xl", "3xl"];
+
+			// Loop through all semantic-props elements adding attributes:
 			for (const entry of entries) {
 				/** Resized semantic-props element. @type {Element} */
 				const element = entry.target;
-	
-				// Reset all breakpoint classes on element:
-				element.classList.remove(...breakpoints);
-	
-				// Loop through all breakpoints and add appropriate classes:
+
+				// Reset all breakpoint attributes on element:
+				breakpoints.forEach(attr => element.removeAttribute(`is-${attr}-breakpoint`));
+
+				// Loop through all breakpoints and add appropriate attributes:
 				for (const prop of breakpoints) {
 					/** Current breakpoint width. @type {string} */
-					const breakpointWidth = getComputedStyle(element).getPropertyValue(prop);
-	
-					// Add breakpoint classes when valid.
+					const breakpointWidth = getComputedStyle(element).getPropertyValue(`--${prop}-breakpoint`);
+
+					// Add breakpoint is attributes when valid:
 					if (entry.borderBoxSize[0].inlineSize >= parseFloat(breakpointWidth)) {
-						element.classList.add(prop);
+						element.setAttribute(`is-${prop}-breakpoint`, "");
 					}
 				}
 			}
 		});
-	
-		/** Adds appropriate color class for user color scheme. */
+
+		/** Adds appropriate color attribute for user color scheme. */
 		static color(component) {
 			/** Media Query for if user prefers dark color scheme. @type {MediaQueryList}  */
 			const darkModeQuery = matchMedia("(prefers-color-scheme: dark)");
-	
+
 			/** Event listener for color scheme matchMedia() event. */
 			const listener = () => {
 				/** Whether user has selected to prefer dark color scheme. @type {boolean} */
 				const isDarkMode = darkModeQuery.matches;
-		
-				// Reset all color classes on container:
-				component.classList.remove("--light-color", "--dark-color");
-		
-				// Set appropriate color class based on preferred colors:
-				component.classList.add(isDarkMode ? "--dark-color" : "--light-color");
+
+				// Reset all color attributes on container:
+				["light-color", "dark-color"].forEach(attr => component.removeAttribute(`is-${attr}`));
+
+				// Set appropriate color attribute based on preferred colors:
+				component.setAttribute(isDarkMode ? "is-dark-color" : "is-light-color", "");
 			};
-	
+
 			// Watch for preferred color scheme changes:
 			darkModeQuery.addEventListener("change", listener);
-	
+
 			// Initially call event listener:
 			listener();
 		}
-	
+
 		/** Executes when web component has connected. */
 		connectedCallback() {
 			// Start observing breakpoints on this component:
 			this.constructor.breakpoint.observe(this);
-	
+
 			// Start listening for color scheme changes for this component:
 			this.constructor.color(this);
 		}
 	}
-	
+
 	// Define "semantic-props" custom element:
 	customElements.define("semantic-props", SemanticProps);
 }
