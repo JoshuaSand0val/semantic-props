dc81afd589afdbc0e0eb912245e7ba32172af7ef
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-07-21 15:22:50 -0600

    Fixed custom property classes not being added to new elements.

--- dist/import.d.ts
+++ dist/import.d.ts
@@ -1,8 +1,7 @@
 export { t as default };
 /**
- * Adds classes to all Semantic Props elements.
+ * Adds custom property classes to all Semantic Props elements.
  * @type {Function}
  * @preserve
- */
-declare function t(): void;
+ */ declare function t(): void;
 //# sourceMappingURL=import.d.ts.map
--- src/import.js
+++ src/import.js
@@ -3,53 +3,30 @@ import breakpoint from "./breakpoint.js";
 import color from "./color.js";
 
 /**
- * Adds classes to all Semantic Props elements.
+ * Adds custom property classes to all Semantic Props elements.
  * @type {Function}
  * @preserve
  */
 export default function semantic() {
-	/**
-	 * Adds Semantic Props classes to element.
-	 * @param {Element} element Element to add Semantic Props classes to.
-	 */
-	const initialize = (element) => {
-		breakpoint(element);
-		color(element);
+	/** Adds Semantic Props classes to elements. */
+	const initialize = () => {
+		for (const element of document.querySelectorAll(".--semantic")) {
+			breakpoint(element);
+			color(element);
+		}
 	};
 
+	// Initialize Semantic Props:
+	initialize();
+
 	/** MutationObserver for adding classes to Semantic Props elements. */
-	const semanticObserver = new MutationObserver(records => {
-		for (const record of records) {
-			/** Potential Semantic Props element. @type {Element} */
-			const element = record.target;
+	const semanticObserver = new MutationObserver(initialize);
 
-			// Continue if class attribute was not mutated:
-			if (record.type !== "attributes") {
-				continue;
-			}
-
-			// Add classes only to new Semantic Props elements:
-			if (!record.oldValue || !record.oldValue.includes("--semantic")) {
-				if (element.classList.contains("--semantic")) {
-					initialize(element);
-				}
-			}
-		}
-	});
-
-	// Observe document for new Semantic Props elements:
+	// Observe document for any mutations to elements:
 	semanticObserver.observe(document.documentElement, {
 		subtree: true,
-		childList: true,
-		attributes: true,
-		attributeFilter: ["class"],
-		attributeOldValue: true
+		childList: true
 	});
-
-	// Add classes to existing Semantic Props elements:
-	for (const element of document.querySelectorAll(".--semantic")) {
-		initialize(element);
-	}
 }
 
 // Attempt to initialize Semantic Props on DOM content load:
