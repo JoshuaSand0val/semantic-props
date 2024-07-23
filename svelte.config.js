7d351d9b154ad04fe897e15f3a39f867560117a8
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-07-22 19:06:23 -0600

    Configured Svelte application.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
+	},
+	preprocess: [vitePreprocess(), sveltePreprocess()]
 };
 
 export default config;
