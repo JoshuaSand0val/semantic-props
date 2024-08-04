56f37917992345147c538049083d05321006c168
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-08-04 02:16:44 -0600

    Fixed breakpoint custom properties not working with non-pixel values.

--- src/breakpoint.js
+++ src/breakpoint.js
@@ -10,6 +10,9 @@ export default function breakpoint(element) {
 		"--xl-breakpoint", "--2xl-breakpoint", "--3xl-breakpoint"
 	];
 
+	/** Element inline CSS min-inline-size state. @type {string|null} */
+	let minInlineSize = null;
+
 	/** Adds appropriate breakpoint class for current container width. @type {ResizeObserver} */
 	const breakpointObserver = new ResizeObserver(entries => {
 		for (const entry of entries) {
@@ -18,18 +21,30 @@ export default function breakpoint(element) {
 
 			// Reset all breakpoint classes on element:
 			element.classList.remove(...breakpoints);
+	
+			/** Element CSS inline-size. @type {string} */
+			const inlineSize = getComputedStyle(element).getPropertyValue("inline-size");
+
+			// Set element inline CSS min-inline-size state:
+			minInlineSize ??= element.style.minInlineSize;
 
 			// Loop through all breakpoints and add appropriate classes:
 			for (const prop of breakpoints) {
-				/** Current breakpoint width. @type {string} */
-				const breakpointWidth = getComputedStyle(element).getPropertyValue(prop);
-
-				// Add breakpoint classes when valid:
-				if (entry.borderBoxSize[0].inlineSize >= parseFloat(breakpointWidth)) {
+				// Set element CSS min-inline-size to calculate remainder from breakpoint:
+				element.style.minInlineSize = `calc(${inlineSize} - var(${prop}))`;
+	
+				// Add class when element is at least breakpoint size:
+				if (getComputedStyle(element).getPropertyValue("min-inline-size") !== "0px") {
 					element.classList.add(prop);
 				}
 			}
-		} 
+
+			// Reset element inline CSS min-inline-size:
+			element.style.minInlineSize = minInlineSize;
+
+			// Nullify min-inline-size inline CSS state:
+			minInlineSize = null;
+		}
 	});
 
 	// Observe resize of all Semantic Props elements:
