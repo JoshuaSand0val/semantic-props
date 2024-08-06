84c68970ba767c12e1a67063d8c54445ef6aa926
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-08-05 18:43:51 -0600

    Rebuilt distributable files for v0.2.1.

--- dist/import.d.ts
+++ dist/import.d.ts
@@ -1,7 +1,7 @@
-export { o as default };
+export { n as default };
 /**
  * Adds custom property classes to all Semantic Props elements.
  * @type {Function}
  * @preserve
- */ declare function o(): void;
+ */ declare function n(): void;
 //# sourceMappingURL=import.d.ts.map
