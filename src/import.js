f5d4c5340986846f2705e7bce7c0c2a1c1bb26f8
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-09-09 02:08:43 -0600

    Changed how deprecated code is handled.

--- src/import.js
+++ src/import.js
@@ -1,12 +1,14 @@
 // Import: All Semantic Props imported.
+
+// Deprecated:
+export { default } from "./deprecated/import.js";
+
+// Supported:
 import "./breakpoints.scss";
 import "./colors.scss";
 import "./containers.scss";
-import "./deprecated.css";
 import "./font.scss";
 import "./layer.css";
 import "./safe.css";
 import "./size.scss";
 import "./timing.css";
-
-export { default } from "./scripts/import.js";
