82fded5de34a43f626312e9ab956e2734af13dea
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-09-11 01:37:14 -0600

    Fixed scroll Semantic Props in non-client environments.

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,16 +1,18 @@
 // Add scroll event that activates Semantic Props on elements:
-document.addEventListener("scroll", () => {
-	// Get all Semantic Props elements:
-	const elements: NodeListOf<HTMLElement> = document.querySelectorAll(".--semantic");
+if (typeof window !== "undefined") {
+	document.addEventListener("scroll", () => {
+		// Get all Semantic Props elements:
+		const elements: NodeListOf<HTMLElement> = document.querySelectorAll(".--semantic");
 
-	// Set scroll Semantic Props styles:
-	for (const el of elements) {
-		// Calculate element scroll X and Y percentages:
-		const scrollXPercentage = el.scrollLeft / (el.scrollWidth - el.offsetWidth) || 0;
-		const scrollYPercentage = el.scrollTop / (el.scrollHeight - el.offsetHeight) || 0;
+		// Set scroll Semantic Props styles:
+		for (const el of elements) {
+			// Calculate element scroll X and Y percentages:
+			const scrollXPercentage = el.scrollLeft / (el.scrollWidth - el.offsetWidth) || 0;
+			const scrollYPercentage = el.scrollTop / (el.scrollHeight - el.offsetHeight) || 0;
 
-		// Set element Semantic Props styles:
-		el.style.setProperty("--scroll-x-percentage", scrollXPercentage.toFixed(2));
-		el.style.setProperty("--scroll-y-percentage", scrollYPercentage.toFixed(2));
-	}
-}, true);
+			// Set element Semantic Props styles:
+			el.style.setProperty("--scroll-x-percentage", scrollXPercentage.toFixed(2));
+			el.style.setProperty("--scroll-y-percentage", scrollYPercentage.toFixed(2));
+		}
+	}, true);
+}
