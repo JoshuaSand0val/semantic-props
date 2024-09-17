a3dd9cc78364d82274d880929c7318bde51b9938
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-09-16 20:53:50 -0600

    Changed range Semantic Props to support all breakpoints.

--- src/range.ts
+++ src/range.ts
@@ -1,18 +1,39 @@
-// Semantic Props used for calculating value ranges.
+// Semantic Props used for calculating breakpoint ranges.
 
 import semantic from "./semantic.js";
+
+/** Semantic Props breakpoints to lengths. */
+const breakpoints: { [key: string]: number } = {
+	"3xs": 0,
+	"2xs": 180,
+	"xs": 240,
+	"sm": 360,
+	"md": 480,
+	"lg": 768,
+	"xl": 1024,
+	"2xl": 1440,
+	"3xl": 1600
+};
 
 /** Calculates range from value, min and max. */
 const range = (min: number, value: number, max: number): string => {
 	return Math.min(Math.max((value - min) / (max - min), 0), 1).toFixed(2);
 };
 
-/** Updates range Semantic Props. */
-const update = () => semantic({
-	"--vw-range": range(160, innerWidth, 1920),
-	"--vh-range": range(160, innerHeight, 1920)
-});
+// Define breakpoint range Semantic Props:
+for (const [start, min] of Object.entries(breakpoints)) {
+	for (const [end, max] of Object.entries(breakpoints)) {
+		// Continue if breakpoints are incompatible:
+		if (min >= max) continue;
 
-// Start updating Semantic Props:
-addEventListener("resize", update);
-update();
+		/** Updates Semantic Props breakpoint range. */
+		const update = () => semantic({
+			[`--${start}-to-${end}-vw`]: range(min, innerWidth, max),
+			[`--${start}-to-${end}-vh`]: range(min, innerHeight, max)
+		});
+
+		// Begin listening for Semantic Props:
+		addEventListener("resize", update);
+		update();
+	}
+}
