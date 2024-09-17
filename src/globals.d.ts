7c6d78af19e622d4276c1d2eeee3c35a7a922cff
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-09-16 20:00:28 -0600

    Changed new color system to use classes. Removed JavaScript exports.

--- src/globals.d.ts
+++
@@ -1,3 +0,0 @@
-// Add CSS modules support to TypeScript:
-declare module "*.module.scss";
-declare module "*.module.css";
--- src/import.ts
+++ src/import.ts
@@ -5,7 +5,7 @@ export { default } from "./deprecated/import.js";
 
 // Supported:
 import "./breakpoints.scss";
-import "./colors.js";
+import "./colors.scss";
 import "./containers.scss";
 import "./font.scss";
 import "./layer.css";
@@ -13,8 +13,6 @@ import "./pointer.js";
 import "./range.js";
 import "./safe.css";
 import "./scroll.js";
-import semantic from "./semantic.js";
+import "./semantic.js";
 import "./size.scss";
 import "./timing.css";
-
-export { semantic };
