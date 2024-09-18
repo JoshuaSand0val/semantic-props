a4fcd1dd171a4c9f2ec7e646238f44c3f277d586
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-09-18 01:17:48 -0600

    Fixed JSDoc output.

--- dist/import.d.ts
+++ dist/import.d.ts
@@ -1,6 +1,17 @@
-declare function o(): void;
+/**
+ * Activates Semantic Props elements, adding custom property classes.
+ * @type {Function}
+ * @preserve
+ */ declare function o(): void;
 
+/** Type for Semantic Props name. */
 type prop = `--${string}`;
+/**
+ * Returns Semantic Props value.
+ * @param prop Semantic Props value to use.
+ * @return Semantic Props value.
+ * @preserve
+ */
 declare const prop: (prop: prop) => string | undefined | Error;
 
 export { o as default, prop };
