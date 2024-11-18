00d2d6bd878514ba6a2ca968a5867b5078d657bc
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-11-17 17:04:28 -0700

    Cleaned up code in preparation for v1.0.0

--- dist/import.d.ts
+++
@@ -1,7 +0,0 @@
-export { o as default };
-/**
- * Activates Semantic Props elements, adding custom property classes.
- * @type {Function}
- * @preserve
- */ declare function o(): void;
-//# sourceMappingURL=import.d.ts.map
