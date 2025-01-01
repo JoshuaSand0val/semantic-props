75b64523d98f28badb18742de7910a0e634566a3
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-01-01 12:45:18 -0700

    Added target prop to Link component.

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,11 +4,12 @@ import "./Link.css";
 /**
  * A link to another webpage.
  * @param props.to String of page link to go to.
+ * @param props.target Link browsing context.
  * @param props.children Content of the link.
  */
 
-export default function Link({ to, children = to}: { to: string, children: ReactNode }) {
+export default function Link({ to, target = "_self", children = to}: { to: string, target: string, children: ReactNode }) {
 	return (
-		<a className="Link" href={to}>{children}</a>
+		<a className="Link" href={to} target={target}>{children}</a>
 	);
 }
