41ece3172054a0f85032e7a91b30828542607636
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-01-01 21:11:17 -0700

    Fixed safe-area padding on header component.

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,27 +4,25 @@ import "./header.css";
 export default function Navbar() {
 	return (
 		<header className="Header container">
-			<div className="Header wrapper">
-				<nav className="Header navigation">
-					<a href="https://github.com/JoshuaSand0val/semantic-props" target="_blank">
-						<i className="bi bi-github"></i>
-						Github
-					</a>
-					<a href="https://heyjes.dev/" target="_blank">
-						<i className="bi bi-person-circle"></i>
-						Hey, Jes!
-					</a>
-				</nav>
-				<hgroup className="Header brand">
-					<p className="Header motto">CSS variables made easy.</p>
-					<h1 className="Header title">
-						<FadeIn text="Semantic Props" />
-					</h1>
-					<span className="Header info">
-						<i className="bi bi-info-circle">&ensp;</i>
-						v1.0.0 (MIT)</span>
-				</hgroup>
-			</div>
+			<nav className="Header navigation">
+				<a href="https://github.com/JoshuaSand0val/semantic-props" target="_blank">
+					<i className="bi bi-github"></i>
+					Github
+				</a>
+				<a href="https://heyjes.dev/" target="_blank">
+					<i className="bi bi-person-circle"></i>
+					Hey, Jes!
+				</a>
+			</nav>
+			<hgroup className="Header brand">
+				<p className="Header motto">CSS variables made easy.</p>
+				<h1 className="Header title">
+					<FadeIn text="Semantic Props" />
+				</h1>
+				<span className="Header info">
+					<i className="bi bi-info-circle">&ensp;</i>
+					v1.0.0 (MIT)</span>
+			</hgroup>
 		</header>
 	);
 }
