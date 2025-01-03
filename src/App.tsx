cca35c7d7f76aca087b4c24998cd23ea85873537
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-01-03 01:16:14 -0700

    Added max height to site navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,28 @@ export default function App() {
 					<main className="App main">
 						<Navbar />
 						<Header />
-
-						<ShowcaseLink to="#blur" title="Blur filters" />
-						<ShowcaseLink to="#border" title="Border properties" />
-						<ShowcaseLink to="#brightness" title="Brightness filters" />
-						<ShowcaseLink to="#color" title="Colors" />
-						<ShowcaseLink to="#container" title="Container Sizes" />
-						<ShowcaseLink to="#contrast" title="Contrast filters" />
-						<ShowcaseLink to="#ease" title="Easing functions" />
-						<ShowcaseLink to="#font" title="Font properties" />
-						<ShowcaseLink to="#letter" title="Letter-spacing properties" />
-						<ShowcaseLink to="#line" title="Line-height properties" />
-						<ShowcaseLink to="#opacity" title="Opacity properties" />
-						<ShowcaseLink to="#ratio" title="Aspect-Ratios" />
-						<ShowcaseLink to="#safe" title="Display safe-areas" />
-						<ShowcaseLink to="#saturation" title="Saturation filters" />
-						<ShowcaseLink to="#scale" title="Scale properties" />
-						<ShowcaseLink to="#shadow" title="Drop-shadow filters" />
-						<ShowcaseLink to="#size" title="Sizing properties" />
-						<ShowcaseLink to="#time" title="Time properties" />
-						<ShowcaseLink to="#word" title="Word-spacing properties" />
-						<ShowcaseLink to="#z" title="Z-index properties" />
+						<nav className="App navigation">
+							<ShowcaseLink to="#blur" title="Blur filters" />
+							<ShowcaseLink to="#border" title="Border properties" />
+							<ShowcaseLink to="#brightness" title="Brightness filters" />
+							<ShowcaseLink to="#color" title="Colors" />
+							<ShowcaseLink to="#container" title="Container Sizes" />
+							<ShowcaseLink to="#contrast" title="Contrast filters" />
+							<ShowcaseLink to="#ease" title="Easing functions" />
+							<ShowcaseLink to="#font" title="Font properties" />
+							<ShowcaseLink to="#letter" title="Letter-spacing properties" />
+							<ShowcaseLink to="#line" title="Line-height properties" />
+							<ShowcaseLink to="#opacity" title="Opacity properties" />
+							<ShowcaseLink to="#ratio" title="Aspect-Ratios" />
+							<ShowcaseLink to="#safe" title="Display safe-areas" />
+							<ShowcaseLink to="#saturation" title="Saturation filters" />
+							<ShowcaseLink to="#scale" title="Scale properties" />
+							<ShowcaseLink to="#shadow" title="Drop-shadow filters" />
+							<ShowcaseLink to="#size" title="Sizing properties" />
+							<ShowcaseLink to="#time" title="Time properties" />
+							<ShowcaseLink to="#word" title="Word-spacing properties" />
+							<ShowcaseLink to="#z" title="Z-index properties" />
+						</nav>
 					</main>
 					<aside className="App aside">
 						Usage &amp; Description
