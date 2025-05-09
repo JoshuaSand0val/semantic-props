8c8a367bb2941a177370e1db6e9008a80dc71bfe
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-08 22:24:31 -0600

    Setup base layout styling.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	let { children } = $props();
+</script>
+
+<div class="container">
+	{@render children()}
+</div>
+
+<style>
+	:global {
+		@import "sanitize.css";
+		@import "semantic-props";
+	}
+
+	:global(:root) {
+		display: block;
+		background-color: var(--primary-background);
+		color: var(--primary-neutral);
+	}
+</style>
