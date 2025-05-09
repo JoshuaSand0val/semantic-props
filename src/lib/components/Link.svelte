ad3f97dfcfce83c7489da7016f350f4159f3b173
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-08 23:37:25 -0600

    Added Link component.

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	interface props {
+		children: Snippet,
+		to: string
+	}
+
+	let { children, to }: props = $props();
+</script>
+
+<a class="container" href={to}>
+	{@render children()}
+</a>
+
+<style>
+	.container {
+		color: var(--primary-accent);
+		&:not(:hover, :focus) {
+			text-decoration: none;
+		}
+		&:active {
+			color: var(--secondary-accent);
+		}
+	}
+</style>
