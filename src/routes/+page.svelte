1b65641a69978ed0f3435420199d914e8ccdd024
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-18 12:16:32 -0600

    Added title gradient stroke animation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,8 +41,30 @@
 	}
 
 	.title {
-		background-image: linear-gradient(132deg, var(--primary-cyan), var(--third-blue));
+		-webkit-text-stroke: 3px transparent;
+		background-image: linear-gradient(123deg,
+			var(--primary-red),
+			var(--primary-orange),
+			var(--primary-yellow),
+			var(--primary-green),
+			var(--primary-blue),
+			var(--primary-indigo),
+			var(--primary-violet)
+		);
 		background-clip: text;
-		color: transparent;
+		color: var(--secondary-white);
+		animation: title 3s infinite both;
+		@media (prefers-reduced-motion: reduce) {
+			animation: none;
+		}
+	}
+
+	@keyframes title {
+		from {
+			filter: hue-rotate(0deg)var(--lightest-shadow);
+		}
+		to {
+			filter: hue-rotate(-360deg) var(--lightest-shadow);
+		}
 	}
 </style>
