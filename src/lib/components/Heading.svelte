047f7457d30e67bfb02907acc5a9558069746bad
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-19 10:15:42 -0600

    Added bottom margin to Heading component.

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -16,7 +16,7 @@
 		transition: font-size var(--fast-time) var(--ease-out);
 		font-family: var(--display-family);
 		line-height: var(--short-line);
-		margin-block: var(--4x-large) 0;
+		margin-block: var(--4x-large) var(--4x-small);
 		:global(& + &) {
 			margin-block-start: 0;
 		}
