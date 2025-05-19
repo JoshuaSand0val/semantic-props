bce20c292b0b48339dc028b061b75d7997137e22
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-19 12:56:07 -0600

    Fixed BuiltWith block max-inline-size.

--- src/lib/blocks/BuiltWith.svelte
+++ src/lib/blocks/BuiltWith.svelte
@@ -5,27 +5,25 @@
 </script>
 
 <Divider title="Built using Semantic Props">
-	<Content>
-		<div class="grid" id="builtwith">
-			<Portal
-				title="Hey, Jes!"
-				description="Portfolio website by Jess Sandoval."
-				url="https://heyjes.dev/"
-				image="built-with/heyjesdev.png"
-				imageDark="built-with/heyjesdev-dark.png"
-			/>
-			<Portal
-				title="Coming Soon"
-				description="Built with Semantic Props."
-				url="#builtwith"
-			/>
-			<Portal
-				title="Coming Soon"
-				description="Built with Semantic Props."
-				url="#builtwith"
-			/>
-		</div>
-	</Content>
+	<div class="grid" id="builtwith">
+		<Portal
+			title="Hey, Jes!"
+			description="Portfolio website by Jess Sandoval."
+			url="https://heyjes.dev/"
+			image="built-with/heyjesdev.png"
+			imageDark="built-with/heyjesdev-dark.png"
+		/>
+		<Portal
+			title="Coming Soon"
+			description="Built with Semantic Props."
+			url="#builtwith"
+		/>
+		<Portal
+			title="Coming Soon"
+			description="Built with Semantic Props."
+			url="#builtwith"
+		/>
+	</div>
 </Divider>
 
 <style>
@@ -35,7 +33,7 @@
 		inline-size: 100vi;
 		gap: var(--small);
 		scroll-snap-type: x mandatory;
-		padding-inline: inherit;
+		padding-inline: calc((100vi - 100%) * 0.5);
 		margin-inline: calc((100vi - 100%) * -0.5);
 		margin-block: var(--2x-large);
 		overflow: auto;
