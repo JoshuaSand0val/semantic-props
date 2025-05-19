7332b69db9a50c4b13e127f8e2fa9e91ad7dc0a8
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-19 10:24:40 -0600

    Changed "project" to "design".

--- src/lib/blocks/Intro.svelte
+++ src/lib/blocks/Intro.svelte
@@ -11,7 +11,7 @@
 			<Heading level={1}>
 				<span class="title">Semantic Props</span>
 			</Heading>
-			<Heading level={6}>Expertly crafted CSS Custom Properties (variables) for creating <strong>consistent components</strong> in any project.</Heading>
+			<Heading level={6}>Expertly crafted CSS Custom Properties (variables) for creating <strong>consistent components</strong> in any design.</Heading>
 			<CodeBlock description="Import At-Rule" lang="css" code='@import "https://unpkg.com/semantic-props";' />
 			<CodeBlock description="Scoping Class" lang="html" code='<html class="semantic">...</html>' />
 		</Content>
