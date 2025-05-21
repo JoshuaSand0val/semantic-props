dbc330ac71c06b98a545b637b6c9beff55135082
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-20 23:09:22 -0600

    Simplified color.css source with PostCSS loops and color-mix function.

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,11 @@ export default {
 	plugins: {
 		"postcss-import": {},
 		"postcss-nesting": {},
+		"postcss-advanced-variables": {},
 		"@csstools/postcss-oklab-function": {},
+		"@csstools/postcss-color-mix-function": {
+			preserve: false
+		},
 		"autoprefixer": {},
 		"cssnano": {
 			preset: "default"
