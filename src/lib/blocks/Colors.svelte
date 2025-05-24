0456ffdff1b41bab1d12d7a41a9cd924dc3ede01
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-24 01:08:45 -0600

    Switched to grid to layout color swatches.

--- src/lib/blocks/Colors.svelte
+++ src/lib/blocks/Colors.svelte
@@ -56,18 +56,14 @@
 
 <style>
 	.container {
-		display: flex;
-		flex-flow: row wrap;
-		align-items: start;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(min(100%, var(--smallest-container)), 1fr));
+		align-items: stretch;
 		gap: var(--2x-small);
 		padding-inline: calc((100vi - 100%) * 0.5);
 		margin-inline: calc((100vi - 100%) * -0.5);
 		scroll-padding: var(--2x-small);
 		scrollbar-width: none;
 		overflow: auto;
-		:global(& > *) {
-			flex: 1 0 var(--smallest-container);
-		}
 	}
-
 </style>
