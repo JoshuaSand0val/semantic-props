36f83d448b3d6c2b24e170382d71bbf588f358df
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-30 09:47:48 -0600

    Wrapped JavaScript code in IIFE.

--- src/color.ts
+++ src/color.ts
@@ -1,26 +1,28 @@
-/** Toggles `light` or `dark` class on Semantic Props containers. */
-function color(): void {
+(function () {
+	if (typeof window === "undefined") return;
+
 	/** All Semantic Props containers. */
 	const semantic: HTMLCollection = document.getElementsByClassName("semantic");
 
-	for (const element of semantic) {
-		/** Semantic Props container `--color-scheme` value. */
-		const scheme: string = getComputedStyle(element).getPropertyValue("--color-scheme");
+	/** Toggles `light` or `dark` class on Semantic Props containers. */
+	function color(): void {
+		for (const element of semantic) {
+			/** Semantic Props container `--color-scheme` value. */
+			const scheme: string = getComputedStyle(element).getPropertyValue("--color-scheme");
 
-		// Enable `light` or `dark` class via scheme:
-		if ("dark" === scheme) {
-			element.classList.remove("light");
-			element.classList.add("dark");
+			// Enable `light` or `dark` class via scheme:
+			if ("dark" === scheme) {
+				element.classList.remove("light");
+				element.classList.add("dark");
+			}
+			else {
+				element.classList.remove("dark");
+				element.classList.add("light");
+			}
 		}
-		else {
-			element.classList.remove("dark");
-			element.classList.add("light");
-		}
+
+		requestAnimationFrame(color);
 	}
 
-	requestAnimationFrame(color);
-}
-
-if (typeof window !== "undefined") {
 	color();
-}
+})();
--- src/size.ts
+++ src/size.ts
@@ -1,25 +1,27 @@
-/** Toggles size classes on Semantic Props containers. */
-function size(): void {
-	/** Semantic Props container sizes. */
-	const sizes: string[] = ["watch", "phone", "tablet", "laptop", "desktop"];
+(function () {
+	if (typeof window === "undefined") return;
 
 	/** All Semantic Props containers. */
 	const semantic: HTMLCollection = document.getElementsByClassName("semantic");
 
-	for (const element of semantic) {
-		/** Semantic Props container computed style value. */
-		const style = getComputedStyle(element);
+	/** Semantic Props container sizes. */
+	const sizes: string[] = ["watch", "phone", "tablet", "laptop", "desktop"];
 
-		// Toggle size classes via boolean props:
-		for (const size of sizes) {
-			const prop: string = style.getPropertyValue(`--${size}`);
-			element.classList.toggle(size, "true" === prop);
+	/** Toggles size classes on Semantic Props containers. */
+	function size(): void {
+		for (const element of semantic) {
+			/** Semantic Props container computed style value. */
+			const style = getComputedStyle(element);
+
+			// Toggle size classes via boolean props:
+			for (const size of sizes) {
+				const prop: string = style.getPropertyValue(`--${size}`);
+				element.classList.toggle(size, "true" === prop);
+			}
 		}
+
+		requestAnimationFrame(size);
 	}
 
-	requestAnimationFrame(size);
-}
-
-if (typeof window !== "undefined") {
 	size();
-}
+})();
