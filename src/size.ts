d7d534f5fe82764c9d4a87b6b7285b6373c476b5
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-05-31 12:52:18 -0600

    Switched to ResizeObserver for toggling container breakpoint classes.

--- src/size.ts
+++ src/size.ts
@@ -1,34 +1,63 @@
 (function () {
 	if (typeof window === "undefined") return;
 
-	/** HTML `:root` document element. */
-	const root: HTMLElement = document.documentElement;
-
-	/** Elements representing a scoped container. */
-	const containers: HTMLCollection = document.getElementsByClassName("container");
-
 	/** Breakpoint class paired to min-width in numbered pixels. */
-	const breakpoints: Record<string, number> = {
+	const breakpoints: [string, number][] = Object.entries({
 		watch: 0,
 		phone: 320,
 		tablet: 640,
 		laptop: 1024,
 		desktop: 1280
-	};
+	});
 
-	/** Toggles matching breakpoint classes on root and containers. */
-	function size(): void {
-		for (const [name, width] of Object.entries(breakpoints)) {
-			for (const container of [root, ...containers]) {
-				if (container instanceof HTMLElement) {
-					container.classList.toggle(name, container.offsetWidth >= width);
+	/** Toggles matching breakpoint classes on entries. */
+	const size = new ResizeObserver(entries => {
+		for (const { target, contentBoxSize } of entries) {
+			if (typeof contentBoxSize[0] === "undefined") continue;
+			for (const [name, minWidth] of breakpoints) {
+				target.classList.toggle(name, contentBoxSize[0].inlineSize >= minWidth);
+			}
+		}
+	});
+
+	/** Starts and stops size observer for containers. */
+	const containers = new MutationObserver(records => {
+		for (const { target, oldValue } of records) {
+			if (!(target instanceof Element)) continue;
+
+			if (target.classList.contains("container")) {
+				if (!oldValue?.includes("container")) {
+					size.observe(target);
+				}
+			}
+			else {
+				if (oldValue?.includes("container")) {
+					size.unobserve(target);
 				}
 			}
 		}
+	});
+
+	/** Initializes observation of all containers. */
+	let initialize = (): void => {
+		// Observe document as container:
+		document.documentElement.classList.add("container");
+
+		// Observe existing containers:
+		for (const container of document.querySelectorAll(".container")) {
+			size.observe(container);
+		}
+
+		// Observe new containers:
+		containers.observe(document.documentElement, {
+			subtree: true,
+			childList: true,
+			attributeFilter: ["class"],
+			attributeOldValue: true
+		});
 	}
 
-	// Initialize breakpoint matching:
-	document.addEventListener("DOMContentLoaded", size);
-	addEventListener("resize", size);
-	size();
+	// Apply initialization:
+	document.addEventListener("DOMContentLoaded", initialize);
+	initialize();
 })();
