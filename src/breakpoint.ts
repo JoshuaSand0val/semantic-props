06da0c845979c1292ede232c9a9439f95f0c53bf
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-06-01 21:07:14 -0600

    Added breakpoint observation of mutated scripted containers.

--- src/breakpoint.ts
+++ src/breakpoint.ts
@@ -21,11 +21,35 @@
 		}
 	});
 
+	/** Controls resize observer for mutated containers. */
+	const mutation = new MutationObserver(records => {
+		for (const { target, oldValue } of records) {
+			if (!(target instanceof Element)) continue;
+
+			if (target.classList.contains("scripted")) {
+				if (!oldValue?.includes("scripted")) {
+					observer.observe(target);
+				}
+			}
+			else if (oldValue?.includes("scripted")) {
+				observer.unobserve(target);
+				target.classList.remove(...breakpoints);
+			}
+		}
+	});
+
 	/** Initializes observation of scripted containers. */
 	function initialize(): void {
 		for (const container of document.querySelectorAll("scripted")) {
 			observer.observe(container);
 		}
+
+		mutation.observe(document.documentElement, {
+			subtree: true,
+			childList: true,
+			attributeFilter: ["class"],
+			attributeOldValue: true
+		});
 	}
 
 	// Initialize scripted containers:
