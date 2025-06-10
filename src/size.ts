d6ab175ec24a352d5d6179e7f9f143cbdef5a014
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-06-09 23:00:44 -0600

    Revert "Restructured props for sizes, timing, transforms and z-index."
    This reverts commit e1a917b462b023613443f0032cedbe037c155851.

--- src/size.ts
+++ src/size.ts
@@ -1,46 +1,32 @@
-/** Semantic Props for sizing containers, type and space. */
+/** Semantic Props for sizing containers, fonts and space. */
 
-interface props {
-	/** Sizes of containers. */
-	container: Record<string, `${number}rem`>
-	/** Sizes for type and space. */
-	size: Record<number, `${number}rem`>
-	/** Smaller relative size. */
-	smaller: `${number}em`
-	/** Larger relative size. */
-	larger: `${number}em`
-}
-
-export const props: props = {
-	container: {
-		smallest: "10rem", // 160px
-		smaller: "15rem", // 240px
-		small: "20rem", // 320px
-		medium: "30rem", // 480px
-		large: "40rem", // 640px
-		larger: "64rem", // 1024px
-		largest: "80rem", // 1280px
-	},
+export const props = {
+	// Container Sizes:
+	smallestContainer: "10rem", // 160px
+	smallerContainer: "15rem", // 240px
+	smallContainer: "20rem", // 320px
+	mediumContainer: "30rem", // 480px
+	largeContainer: "40rem", // 640px
+	largerContainer: "64rem", // 1024px
+	largestContainer: "80rem", // 1280px
 
 	// Base Sizes:
-	size: {
-		[-5]: "0.125rem", // 2px
-		[-4]: "0.25rem", // 4px
-		[-3]: "0.5rem", // 8px
-		[-2]: "0.75rem", // 12px
-		[-1]: "0.875rem", // 14px
-		[0]: "1rem", // 16px
-		[1]: "1.125rem", // 18px
-		[2]: "1.25rem", // 20px
-		[3]: "1.5rem", // 24px
-		[4]: "1.875rem", // 30px
-		[5]: "2.25rem", // 36px
-		[6]: "3rem", // 48px
-		[7]: "3.75rem", // 60px
-		[8]: "4.5rem", // 72px
-		[9]: "6rem", // 96px
-		[10]: "8rem", // 128px
-	},
+	xSmall4: "0.125rem", // 2px
+	xSmall3: "0.25rem", // 4px
+	xSmall2: "0.5rem", // 8px
+	xSmall: "0.75rem", // 12px
+	small: "0.875rem", // 14px
+	medium: "1rem", // 16px
+	large: "1.125rem", // 18px
+	xLarge: "1.25rem", // 20px
+	xLarge2: "1.5rem", // 24px
+	xLarge3: "1.875rem", // 30px
+	xLarge4: "2.25rem", // 36px
+	xLarge5: "3rem", // 48px
+	xLarge6: "3.75rem", // 60px
+	xLarge7: "4.5rem", // 72px
+	xLarge8: "6rem", // 96px
+	xLarge9: "8rem", // 128px
 
 	smaller: "0.875em", // 14/16px
 	larger: "1.125em" // 18/16px
--- src/timing.ts
+++ src/timing.ts
@@ -1,31 +1,18 @@
 /** Semantic Props for timing animations. */
 
-interface props {
-	/** Timing function for easing. */
-	ease: string,
-	/** Timing function for easing inward. */
-	easeIn: string,
-	/** Timing function for easing outward. */
-	easeOut: string,
-	/** Timing function for easing in/outward. */
-	easeInOut: string,
-	/** Duration for timing or delaying animations. */
-	time: Record<string, `${number}ms`>
-}
-
-export const props: props = {
+export const props = {
+	// Easing Values:
 	ease: "ease",
 	easeIn: "ease-in",
 	easeOut: "ease-out",
 	easeInOut: "ease-in-out",
 
-	time: {
-		fastest: "75ms",
-		faster: "100ms",
-		fast: "150ms",
-		normal: "300ms",
-		slow: "500ms",
-		slower: "750ms",
-		slowest: "1000ms"
-	}
+	// Time Values:
+	fastestTime: "75ms",
+	fasterTime: "100ms",
+	fastTime: "150ms",
+	normalTime: "300ms",
+	slowTime: "500ms",
+	slowerTime: "750ms",
+	slowestTime: "1s"
 };
