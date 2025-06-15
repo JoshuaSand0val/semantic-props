648dc6bb23b627b1928158df7d187d3e8fd622b1
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-06-15 16:26:45 -0600

    Revert "Changed from JSON to JavaScript export."
    This reverts commit 0a86147a48e3b1503f0c9607a38db02ffee1a3aa.

--- src/export.ts
+++ src/export.ts
@@ -1,15 +1,16 @@
 // Export: Export Semantic Props to distributable file.
 
-export * as default from "./import";
-
-import { writeFileSync } from "node:fs";
+import { writeFileSync, type WriteFileOptions } from "node:fs";
 import * as props from "./import";
 
 /** JSON export data. */
 const data: string = JSON.stringify(props);
 
-// Write JSON data to distribution file:
-writeFileSync("dist/export.js", `export default ${data}`, {
+/** Options for Node.js `writeFileSync`. */
+const options: WriteFileOptions = {
 	encoding: "utf8",
 	flag: "w"
-});
+};
+
+// Write JSON data to distribution file:
+writeFileSync("dist/export.json", data, options);
