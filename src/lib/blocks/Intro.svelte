cb9339a8d102a827a22e3c915b0f90166c27a514
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-06-30 14:59:02 -0600

    Removed gradient stroke from Intro title.

--- src/lib/blocks/Intro.svelte
+++ src/lib/blocks/Intro.svelte
@@ -33,42 +33,6 @@
 	}
 
 	.title {
-		-webkit-text-stroke: var(--4x-small) transparent;
-		background-image: linear-gradient(123deg,
-			var(--red-600),
-			var(--orange-600),
-			var(--yellow-600),
-			var(--green-600),
-			var(--blue-600),
-			var(--indigo-600),
-			var(--violet-600)
-		);
-		background-clip: text;
-		color: var(--background-color);
 		filter: var(--lightest-shadow);
-		animation: title 3s infinite both;
-		@media (prefers-reduced-motion: reduce) {
-			animation: none;
-		}
-		@media (--dark) {
-			background-image: linear-gradient(123deg,
-				var(--red-300),
-				var(--orange-300),
-				var(--yellow-300),
-				var(--green-300),
-				var(--blue-300),
-				var(--indigo-300),
-				var(--violet-300)
-			);	
-		}
-	}
-
-	@keyframes title {
-		from {
-			filter: hue-rotate(0deg) var(--lightest-shadow);
-		}
-		to {
-			filter: hue-rotate(-360deg) var(--lightest-shadow);
-		}
 	}
 </style>
