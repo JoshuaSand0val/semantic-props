557688d3aef4dc1b30175c0cfdb301ce5cf6dfb6
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-07-05 04:03:45 -0600

    Replaced/Updated color swatches.

---
+++ src/lib/components/ColorPalette.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	/** Semantic Props color palettes. */
+	const palettes: Record<string, `var(${string})`>[] = [{
+		"White": "var(--white)",
+		"Black": "var(--black)"
+	}, {
+		"Background Color": "var(--background-color)",
+		"Middleground Color": "var(--middleground-color)",
+		"Foreground Color": "var(--foreground-color)"
+	}, {
+		"High Contrast Color": "var(--high-contrast-color)",
+		"Medium Contrast Color": "var(--medium-contrast-color)",
+		"Low Contrast Color": "var(--low-contrast-color)"
+	}];
+
+	Object.entries({
+		"Accent": "accent",
+		"Blue": "blue",
+		"Brown": "brown",
+		"Coral": "coral",
+		"Cyan": "cyan",
+		"Gray": "gray",
+		"Green": "green",
+		"Indigo": "indigo",
+		"Lime": "lime",
+		"Magenta": "magenta",
+		"Orange": "orange",
+		"Pink": "pink",
+		"Red": "red",	
+		"Violet": "violet",
+		"Yellow": "yellow"
+	}).forEach(([color, prefix]) => {
+		const weights: Record<string, `var(${string})`> = {};
+
+		for (let weight = 50; weight <= 950; weight += 50) {
+			weights[`${color} ${weight}`] = `var(--${prefix}-${weight})`;
+		}
+
+		palettes.push(weights);
+	});
+</script>
+
+<div class="container">
+	{#each palettes as palette}
+	<dl class="palette">
+		{#each Object.entries(palette) as [color, prop]}
+		<span class="color" style:--prop={prop}>
+			<dt class="name">{color}</dt>
+			<dd class="prop">{prop}</dd>
+		</span>
+		{/each}
+	</dl>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: start;
+		align-items: stretch;
+		gap: var(--x-small);
+		overflow: auto;
+	}
+
+	.palette {
+		display: flex;
+		flex-flow: row wrap;
+		inline-size: min(100%, var(--medium-container));
+		border-radius: var(--smaller-radius);
+		border: 1px solid oklch(0 0 0 / 0.2);
+		margin-block: 0;
+		overflow: hidden;
+		flex: 1 0 auto;
+	}
+
+	.color {
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: space-between;
+		align-content: start;
+		gap: 0 var(--x-small);
+		min-inline-size: min-content;
+		background-color: var(--prop);
+		padding-inline: var(--x-small);
+		padding-block: var(--2x-small);
+		flex: 1 0 0;
+		cursor: pointer;
+		@media (--dark) {
+			border-color: oklch(1 0 0 / 0.1);
+		}
+	}
+
+	.name, .prop {
+		display: block;
+		font-size: var(--small);
+		filter: invert(1) saturate(0) contrast(9999);
+		color: var(--prop);
+		margin: 0;
+	}
+
+	.name {
+		font-family: var(--display-family);
+	}
+
+	.prop {
+		white-space: nowrap;
+		font-family: var(--mono-family);
+		opacity: var(--high-opacity);
+	}
+</style>
--- src/routes/chapters/Colors.svelte
+++ src/routes/chapters/Colors.svelte
@@ -1,106 +1,12 @@
 <script lang="ts">
-    import ColorSwatch from "$lib/components/ColorSwatch.svelte";
+    import ColorPalette from "$lib/components/ColorPalette.svelte";
     import Divider from "$lib/components/Divider.svelte";
     import Paragraph from "$lib/components/Paragraph.svelte";
-
-	/** White/Black Swatch. */
-	const whiteBlack: Record<string, string> = {
-		"White": "--white",
-		"Black": "--black"
-	};
-
-	/** Returns ColorSwatch value, weights 50 to 950. */
-	const weights = (color: string, prefix: string): Record<string, string> => {
-		let swatch = {};
-		for (let weight = 50; weight <= 950; weight += 50) {
-			swatch = { ...swatch, [`${color} ${weight}`]: `${prefix}-${weight}` };
-		}
-		return swatch;
-	};
-
-	/** Color Palette Swatches. */
-	const palette: Record<string, string>[] = [
-		weights("Accent", "--accent"),
-		weights("Blue", "--blue"),
-		weights("Brown", "--brown"),
-		weights("Coral", "--coral"),
-		weights("Cyan", "--cyan"),
-		weights("Gray", "--gray"),
-		weights("Green", "--green"),
-		weights("Indigo", "--indigo"),
-		weights("Lime", "--lime"),
-		weights("Magenta", "--magenta"),
-		weights("Orange", "--orange"),
-		weights("Pink", "--pink"),
-		weights("Red", "--red"),	
-		weights("Violet", "--violet"),
-		weights("Yellow", "--yellow")
-	];
-
-	/** Layer Contextual Swatch. */
-	const contextualLayer: Record<string, string> = {
-		"Background Color": "--background-color",
-		"Middleground Color": "--middleground-color",
-		"Foreground Color": "--foreground-color"
-	};
-
-	/** Contrast Contextual Swatch. */
-	const contextualContrast: Record<string, string> = {
-		"High Contrast Color": "--high-contrast-color",
-		"Medium Contrast Color": "--medium-contrast-color",
-		"Low Contrast Color": "--low-contrast-color"
-	};
 </script>
 
 <Divider title="Color Palette" id="colors">
 	<Paragraph>
 		Semantic Props provides a simple but expansive color palette.
 	</Paragraph>
-	<div class="container scroll">
-		<div class="group">
-			{#each [whiteBlack, contextualLayer, contextualContrast] as swatch}
-			<ColorSwatch colors={swatch}/>
-			{/each}
-		</div>
-		{#each palette as swatch}
-		<ColorSwatch colors={swatch}/>
-		{/each}
-	</div>
+	<ColorPalette />
 </Divider>
-
-<style>
-	@import "$lib/styles/scroll.css";
-
-	.container {
-		--column-width: var(--smallest-container);
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(min(100%, var(--column-width)), 1fr));
-		align-items: stretch;
-		gap: var(--2x-small);
-		padding-inline: calc((100vi - 100%) * 0.5);
-		margin-inline: calc((100vi - 100%) * -0.5);
-		margin-block: var(--2x-large);
-		@media (--laptop) {
-			--column-width: var(--small-container);
-			display: flex;
-			flex-flow: row nowrap;
-			scroll-snap-type: inline mandatory;
-			:global(& > *) {
-				flex: 0 0 var(--column-width);
-				scroll-snap-align: center;
-			}
-		}
-	}
-
-	.group {
-		display: contents;
-		flex-flow: row wrap;
-		gap: inherit;
-		@media (--laptop) {
-			display: flex;
-		}
-		:global(& > *) {
-			flex: 1 1 auto;
-		}
-	}
-</style>
