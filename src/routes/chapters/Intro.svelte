d04756666b14ad20978a842a758159abd46897b8
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-10-12 02:51:08 -0600

    Removed Divider from Intro chapter component.

--- src/routes/chapters/Intro.svelte
+++ src/routes/chapters/Intro.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
 	import CodeBlock from "$lib/components/CodeBlock.svelte";
-    import Divider from "$lib/components/Divider.svelte";
 	import Heading from "$lib/components/Heading.svelte";
 </script>
 
-<header class="container">
-	<Divider title="Introduction to Semantic Props" id="intro">
-		<Heading level={4}>Stylesheet values made simple.</Heading>
-		<Heading level={1}>
-			<span class="title">Semantic Props</span>
-		</Heading>
-		<Heading level={6}>Expertly crafted CSS Custom Properties (variables) for creating <strong>consistent components</strong> in any design.</Heading>
-		<CodeBlock description="Import At-Rule" lang="css" code='@import "https://unpkg.com/semantic-props";' />
-	</Divider>
+<header class="container" id="intro">
+	<Heading level={4}>Stylesheet values made simple.</Heading>
+	<Heading level={1}>
+		<span class="title">Semantic Props</span>
+	</Heading>
+	<Heading level={6}>Expertly crafted CSS Custom Properties (variables) for creating <strong>consistent components</strong> in any design.</Heading>
+	<CodeBlock description="Import At-Rule" lang="css" code='@import "https://unpkg.com/semantic-props";' />
 </header>
 
 <style>
