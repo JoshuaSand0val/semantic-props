2686e12656980fd81de9ebfde0c017b942bdcfe1
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-04 11:05:50 -0700

    Improved design of size demos.

--- src/routes/chapters/Sizes.svelte
+++ src/routes/chapters/Sizes.svelte
@@ -2,119 +2,102 @@
     import Divider from "$lib/components/Divider.svelte";
 
 	const containers: Record<string, string>= {
-		"Smallest Container": "--smallest-container",
-		"Smaller Container": "--smaller-container",
-		"Small Container": "--small-container",
-		"Medium Container": "--medium-container",
-		"Large Container": "--large-container",
-		"Larger Container": "--larger-container",
-		"Largest Container": "--largest-container"
+		"Smallest Container": "var(--smallest-container)",
+		"Smaller Container": "var(--smaller-container)",
+		"Small Container": "var(--small-container)",
+		"Medium Container": "var(--medium-container)",
+		"Large Container": "var(--large-container)",
+		"Larger Container": "var(--larger-container)",
+		"Largest Container": "var(--largest-container)"
 	};
 
 	const sizes: Record<string, string> = {
-		"Extra Small (4x)": "--4x-small",
-		"Extra Small (3x)": "--3x-small",
-		"Extra Small (2x)": "--2x-small",
-		"Extra Small": "--x-small",
-		"Small": "--small",
-		"Medium": "--medium",
-		"Large": "--large",
-		"Extra Large": "--x-large",
-		"Extra Large (2x)": "--2x-large",
-		"Extra Large (3x)": "--3x-large",
-		"Extra Large (4x)": "--4x-large",
-		"Extra Large (5x)": "--5x-large",
-		"Extra Large (6x)": "--6x-large",
-		"Extra Large (7x)": "--7x-large",
-		"Extra Large (8x)": "--8x-large",
-		"Extra Large (9x)": "--9x-large",
+		"Extra Small (4x)": "var(--4x-small)",
+		"Extra Small (3x)": "var(--3x-small)",
+		"Extra Small (2x)": "var(--2x-small)",
+		"Extra Small": "var(--x-small)",
+		"Small": "var(--small)",
+		"Medium": "var(--medium)",
+		"Large": "var(--large)",
+		"Extra Large": "var(--x-large)",
+		"Extra Large (2x)": "var(--2x-large)",
+		"Extra Large (3x)": "var(--3x-large)",
+		"Extra Large (4x)": "var(--4x-large)",
+		"Extra Large (5x)": "var(--5x-large)",
+		"Extra Large (6x)": "var(--6x-large)",
+		"Extra Large (7x)": "var(--7x-large)",
+		"Extra Large (8x)": "var(--8x-large)",
+		"Extra Large (9x)": "var(--9x-large)",
 		
-		"Smaller": "--smaller",
-		"Larger": "--larger",
+		"Smaller": "var(--smaller)",
+		"Larger": "var(--larger)",
 
-		"Margin Size": "--margin-size"
+		"Margin Size": "var(--margin-size)"
 	};
 </script>
 
 <Divider title="Containers and Spacing Sizes" id="sizes">
 	<p>Semantic Props provides containers alongside sizes.</p>
-	<div class="size-grid">
-		{#each Object.entries(sizes) as [size, prop]}
-		<span class="size" style:--prop={`var(${prop})`}>
-			<strong class="title">{size}</strong>
-			<small class="prop">var({prop})</small>
-		</span>
-		{/each}
-	</div>
-	<div class="container-column">
-		{#each Object.entries(containers) as [container, prop]}
-		<span class="container" style:--prop={`var(${prop})`}>
-			<strong class="title">{container}</strong>
-			<small class="prop">var({prop})</small>
-		</span>
+	<div class="scroller">
+		{#each Object.entries({ ...sizes, ...containers }) as [title, prop]}
+		<div class="demo" style:--prop={prop}>
+			<span class="size"></span>
+			<span>
+				<strong class="title">{title}</strong>
+				<small class="prop">{prop}</small>
+			</span>
+		</div>
 		{/each}
 	</div>
 </Divider>
 
 <style>
-	.size-grid, .container-column {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(min(100%, 8rem, var(--smallest-container)), 1fr));
-		align-items: end;
-		gap: var(--x-small) var(--medium);
-		margin-block: var(--3x-large);
+	.scroller {
+		display: block;
+		writing-mode: sideways-lr;
+		block-size: 100%;
+		margin-inline: var(--3x-large);
+		scroll-snap-type: block mandatory;
 		overflow: auto;
 	}
 
-	.container-column {
+	.demo {
 		display: flex;
-		flex-flow: column nowrap;
+		flex-flow: row nowrap;
 		align-items: start;
-		gap: var(--x-small);
+		justify-content: start;
+		padding: var(--3x-small);
+		gap: var(--small);
+		scroll-snap-align: start;
 	}
 
-	.size, .container {
-		position: relative;
-		display: flex;
-		flex-flow: column nowrap;
-		gap: 0 var(--x-small);
-		line-height: var(--short-line);
-		&::after {
-			content: "";
-			display: block;
-			block-size: min(var(--large), var(--larger));
-			border: 1px dotted var(--low-contrast-color);
-			box-shadow: var(--lightest-box-shadow);
-			background-image: linear-gradient(
-				var(--high-contrast-color),
-				var(--low-contrast-color)
-			);
-			background-size: var(--prop) 100%;
-			margin-block: var(--x-small);
-		}
-	}
-
-	.size::after {
-		inline-size: var(--9x-large);
-	}
-	
-	.container::after {
-		inline-size: var(--largest-container);
+	.title, .prop {
+		display: block;
+		white-space: nowrap;
+		flex: 0 1 auto;
 	}
 
 	.title {
-		display: block;
+		line-height: var(--shorter-line);
 		font-family: var(--display-family);
 		font-weight: var(--bold-weight);
 		color: var(--medium-contrast-color);
-		font-size: var(--medium);
+		font-size: var(--small);
 	}
 
 	.prop {
-		display: block;
 		font-family: var(--mono-family);
 		font-weight: var(--regular-weight);
 		color: var(--low-contrast-color);
-		font-size: var(--small);
+		font-size: var(--x-small);
+	}
+
+	.size {
+		display: block;
+		inline-size: var(--3x-small);
+		padding-block-end: var(--prop);
+		outline: 1px dotted var(--medium-contrast-color);
+		margin-block: var(--4x-small);
+		flex: 0 0 auto;
 	}
 </style>
