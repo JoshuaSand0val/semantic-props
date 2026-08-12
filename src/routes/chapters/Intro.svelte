90cb06b83dbd241e7f071b19a0749fea4597b1c1
Jess Sandoval <me@heyjes.dev>
2026-08-12 13:00:24 -0600

    Updated website to reflect Semantic Props v2.1.1.

--- src/routes/chapters/Intro.svelte
+++ src/routes/chapters/Intro.svelte
@@ -19,7 +19,7 @@
 			<span style:--phrase={5}>in any design.</span>
 		</span>
 	</Heading>
-	<CodeBlock description="Import At-Rule" lang="css" code='@import "https://cdn.jsdelivr.net/npm/semantic-props@2.1.0";' />
+	<CodeBlock description="Import At-Rule" lang="css" code='@import "https://cdn.jsdelivr.net/npm/semantic-props@2.1.1";' />
 	<CodeBlock description="NPM Install Command" lang="shell" code='npm install semantic-props' />
 </header>
 
